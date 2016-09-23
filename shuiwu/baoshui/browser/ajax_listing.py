06f4516ff04b58d41f3d4befadbdcf522851c181
adam139 <yuejun.tang@gmail.com>
2016-09-23 20:34:50 +0800

    fix pager and type registry

--- shuiwu/baoshui/browser/ajax_listing.py
+++ shuiwu/baoshui/browser/ajax_listing.py
@@ -392,7 +392,7 @@ class ajaxsearch(grok.View):
                                 <td class="col-md-3 text-left"><a href="%(objurl)s">%(title)s</a></td>
                                 <td class="col-md-7">%(description)s</td>
                                 <td class="col-md-1 text-center">%(date)s</td>                                
-                            </tr> """% dict(objurl="%s/view" % i.getURL(),
+                            </tr> """% dict(objurl="%s" % i.getURL(),
                                             num=str(k + 1),
                                             title=i.Title,
                                             description= i.Description,
--- shuiwu/baoshui/indexer.py
+++ shuiwu/baoshui/indexer.py
@@ -5,3 +5,7 @@ from shuiwu.baoshui.content.nashuiren import Inashuiren
 @indexer(Inashuiren)
 def indexer_guanlidaima(obj, **kw):
     return obj.guanlidaima
+
+@indexer(Inashuiren)
+def indexer_dengjiriqi(obj, **kw):
+    return obj.dengjiriqi
