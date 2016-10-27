da3e745ae56ae8d6b25c89acaa65a33707b83ccf
Adam <yuejun.tang@gmail.com>
2016-10-27 10:33:24 +0800

    fix ajax table output

--- shuiwu/baoshui/browser/ajax_listing.py
+++ shuiwu/baoshui/browser/ajax_listing.py
@@ -387,7 +387,7 @@ class ajaxsearch(grok.View):
                                 <td class="col-md-1 text-center">%(num)s</td>
                                 <td class="col-md-3 text-left"><a href="%(objurl)s">%(title)s</a></td>
                                 <td class="col-md-3">%(shibiehao)s</td>
-                                <td class="col-md-7">%(description)s</td>
+                                <td class="col-md-4">%(description)s</td>
                                 <td class="col-md-1 text-center">%(date)s</td>                                
                             </tr> """% dict(objurl="%s" % i.getURL(),
                                             num=str(k + 1),
